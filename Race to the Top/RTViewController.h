8e45f84e9fed772d74799d8de94f403d9118df72
Tom Pullen <tpullen21@gmail.com>
2014-12-10 19:09:57 +0000

    Initial Commit

---
+++ Race to the Top/RTViewController.h
@@ -0,0 +1,14 @@
+//
+//  RTViewController.h
+//  Race to the Top
+//
+//  Created by Eliot Arntz on 11/6/13.
+//  Copyright (c) 2013 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RTViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UILabel *scoreLabel;
+
+@end
